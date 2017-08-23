10a3ffc503d24de9ce901d38d9237472873b79eb
Unknown <blooop@gmail.com>
2017-08-23 17:04:56 +0100

    added submodules

--- __init__.py
+++ __init__.py
@@ -1,6 +1,1 @@
-from mathFunc import *
-from timer import *
-from IO import *
-from matplotlibfuncs import *
-from pandasfuncs import *
-from opencvfuncs import *
+
---
+++ mathfuncs/__init__.py
@@ -0,0 +1,1 @@
+from mathfuncs import *
