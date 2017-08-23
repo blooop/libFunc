4587341688df59e3bd12395a55b59438f0ba022e
Unknown <blooop@gmail.com>
2017-08-23 17:48:32 +0100

    added basic trig funcs

--- mathfuncs/mathfuncs.py
+++ mathfuncs/mathfuncs.py
@@ -1,12 +1,22 @@
-def func1(arg1):
-    print "func1"
-    print arg1
+import math
 
-def func2(arg1):
-    print "func2"
+PIB2 = math.pi / 2.0
+PIB4 = math.pi /4.0
+PI = math.pi
+PI2 = math.pi * 2.0
 
-def func3():
-    print "func3"
+deg2rad = 180.0/math.pi
+rad2deg = math.pi/180.0
 
-def func4():
-    print "func4"
+
+def v2a(vec):
+    return math.atan2(vec[1],vec[0])
+
+def v2ad(vec):
+    return math.atan2(vec[1],vec[0])*deg2rad
+
+def vecLen(vec):
+    return math.sqrt(vec[0]*vec[0]+vec[1]*vec[1])
+
+def lerp(value, inputLow, inputHigh, outputLow, outputHigh):
+    return outputLow + ((value - inputLow) / (inputHigh - inputLow)) * (outputHigh - outputLow)
