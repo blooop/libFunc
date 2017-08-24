af8eda87c2b90f8b6dd36507252d0cfe85ab9e17
Unknown <blooop@gmail.com>
2017-08-24 18:56:57 +0100

    autoformat

--- util/import_submodule.py
+++ util/import_submodule.py
@@ -2,5 +2,5 @@ def addProjToSysPath(projName):
     import os
     import sys
     path = os.path.realpath(__file__)
-    projectPath = path[:path.find(projName)]+projName
+    projectPath = path[:path.find(projName)] + projName
     sys.path.append(projectPath)
